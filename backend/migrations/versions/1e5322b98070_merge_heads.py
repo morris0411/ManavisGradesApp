dddc44ce2802e34f4f37c70f14bf912844fe32c2
morris0411 <u583252j@ecs.osaka-u.ac.jp>
2025-11-20 21:10:08 +0900

    migrationsのバグ修正

---
+++ backend/migrations/versions/1e5322b98070_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 1e5322b98070
+Revises: 6e1888cc3b49, a1b2c3d4e5f6
+Create Date: 2025-11-20 21:04:20.841004
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1e5322b98070'
+down_revision = ('6e1888cc3b49', 'a1b2c3d4e5f6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
